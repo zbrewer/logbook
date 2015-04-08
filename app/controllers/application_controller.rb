95aeebbbd17e1b124925373e9ade74890c918f49
Zach Brewer <zbrewer2@illinois.edu>
2015-04-08 00:07:01 -0500

    Can now edit all user data fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up).push(:first_name, :last_name, :birth_date, :certificate_number, :certificate_type, :certificate_issue_date)
+    devise_parameter_sanitizer.for(:account_update).push(:first_name, :last_name, :birth_date, :certificate_number, :certificate_type, :certificate_issue_date)
   end
+
 end
