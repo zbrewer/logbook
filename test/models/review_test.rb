be48266c7f40ea13589c803af606c30c73a0ddda
Zach Brewer <zbrewer2@illinois.edu>
2015-04-09 03:39:26 -0500

    Added basic tests for the review model

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -4,4 +4,23 @@ class ReviewTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "flight review" do
+    review = reviews(:test_flight_review)
+    assert_equal Date.new(2015, 4, 8), review.review_date
+    assert_equal "Harry Potter", review.name_of_reviewer
+    assert review.flight_review
+    assert_not review.instrument_proficiency_check
+    assert_not review.cfi_renewal
+  end
+
+  test "review defaults" do
+    review = reviews(:review_defaults)
+    assert_equal Date.new(2015, 4, 8), review.review_date
+    assert_equal "Hagrid", review.name_of_reviewer
+    assert_not review.flight_review
+    assert_not review.instrument_proficiency_check
+    assert_not review.cfi_renewal
+  end
+
 end
