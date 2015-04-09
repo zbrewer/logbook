781730b4c79c9c8830a78eba39b21ddbb565b96c
Zach Brewer <zbrewer2@illinois.edu>
2015-04-09 03:32:35 -0500

    Wrote basic tests for the medical model. They don't work because it doesn't like the class column for some reason. This needs to be fixed.

--- test/models/medical_test.rb
+++ test/models/medical_test.rb
@@ -4,4 +4,19 @@ class MedicalTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "first class medical" do
+    medical = medicals(:first_class)
+    assert_equal Date.new(2012, 7, 15), medical.exam_date
+    assert_equal 1, medical.class
+    assert_equal "George Washington", medical.examiner_name
+  end
+
+  test "third class medical" do
+    medical = medicals(:third_class)
+    assert_equal Date.new(2013, 2, 21), medical.exam_date
+    assert_equal 3, medical.class
+    assert_equal "Thomas Jefferson", medical.examiner_name
+  end
+
 end
