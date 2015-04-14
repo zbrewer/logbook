2fa966bb87aa7869ab1b290a3a5be442bce2ae73
Zach Brewer <zbrewer02@gmail.com>
2015-04-14 12:06:32 -0500

    Added a controller for airplanes and started working on the form for adding airplanes

---
+++ app/controllers/airplanes_controller.rb
@@ -0,0 +1,4 @@
+class AirplanesController < ApplicationController
+   def new
+   end
+end
---
+++ test/controllers/airplanes_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AirplanesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
