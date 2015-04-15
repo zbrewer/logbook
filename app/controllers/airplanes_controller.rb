7f3fe1e7fe2358ffc3bd8388e208565dbaeb78bc
Zach Brewer <zbrewer2@illinois.edu>
2015-04-14 20:12:58 -0500

    Finished up the form and controller for adding airplanes. Added the view for viewing all airplanes but still need to write it and the associated controller

--- app/controllers/airplanes_controller.rb
+++ app/controllers/airplanes_controller.rb
@@ -1,4 +1,24 @@
 class AirplanesController < ApplicationController
+  before_action :authenticate_user!
+
    def new
    end
+
+   def create
+     @new_airplane = Airplane.new(airplane_params)
+     @new_airplane.user = current_user
+     @new_airplane.save
+     redirect_to action: "index"
+   end
+
+   def index
+   end
+
+   private
+    def airplane_params
+      params.require(:airplane).permit(:identification, :complex, :high_performance, :tailwheel, :high_altitude,
+                                       :turbine, :glider, :rotorcraft, :powered_lift, :lighter_than_air,
+                                       :single_engine_land, :single_engine_sea, :multi_engine_land,
+                                       :multi_engine_sea, :simulator, :display_name, :airplane_type)
+    end
 end
