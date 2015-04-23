d8bd5ea69bf4e17407ef3a33fc5455deb6523d3f
Zach Brewer <zbrewer2@illinois.edu>
2015-04-23 03:15:17 -0500

    Added instrument currency information

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,6 +31,11 @@ class User < ActiveRecord::Base
 
 
 
+  # Include the date helpers (lib/date_helpers.rb)
+  include DateHelpers
+
+
+
   # Function that gets the user's most recent medical
   def current_medical
     self.medicals.order(:exam_date).reverse[0]
@@ -120,6 +125,54 @@ class User < ActiveRecord::Base
   # Returns the last date during which the user is night current
   def last_night_current_date
     User.last_landing_current_date(oldest_night_currency_flight)
+  end
+
+
+
+  # Returns the date of the oldest flight for instrument currency
+  def oldest_instrument_currency_flight
+    flight_list = self.flights.order(:flight_date).reverse
+    total_approaches = 0
+    total_holds = 0
+
+    for flight in flight_list do
+      total_approaches += flight.number_approaches
+      total_holds += flight.number_holds
+
+      if (total_approaches >= 6 and total_holds >= 1) or (flight.review != nil and flight.review.instrument_proficiency_check) or (flight.checkride != nil and flight.checkride.instrument)
+        return flight.flight_date
+      end
+    end
+
+    return nil
+  end
+
+
+
+  # Returns the last date that the user is instrument current
+  # TODO - Get this to always be nil if the user isn't instrument rated
+  def last_instrument_current_date
+    oldest_currency_flight = self.oldest_instrument_currency_flight
+
+    if oldest_currency_flight == nil
+      return nil
+    else
+      return DateHelpers.calendar_months_from(oldest_currency_flight, 6)
+    end
+  end
+
+
+
+  # Returns whether or not the user is instrument current
+  # TODO - Make this always false if the user isn't instrument rated?
+  def instrument_current?
+    last_current_date = self.last_instrument_current_date
+
+    if last_current_date == nil
+      return false
+    else
+      return last_current_date >= Date.today
+    end
   end
 
 end
