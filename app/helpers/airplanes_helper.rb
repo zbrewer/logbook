32e3a9b6a39126449bea2980f3766337987955df
Zach Brewer <zbrewer2@illinois.edu>
2015-04-30 06:36:42 -0500

    Did even more styling including adding some icons

--- app/helpers/airplanes_helper.rb
+++ app/helpers/airplanes_helper.rb
@@ -1,2 +1,15 @@
 module AirplanesHelper
+
+  # Helper function that displays a check if the boolean is true
+  # and an X if the boolean is false
+  def display_boolean_icon(value)
+    if value
+      content_tag(:i, :class => "fa fa-check text-success") do
+      end
+    else
+      content_tag(:i, :class => "fa fa-times text-danger") do
+      end
+    end
+  end
+
 end
